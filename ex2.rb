76d4995712dc594786379d479c3f5f647204c3e4
Anthony Riley <anthony.riley@artemis.net>
2015-02-01 21:04:03 -0800

    First commit of the first 3 exercises

---
+++ ex2.rb
@@ -0,0 +1,8 @@
+# just a comment 
+# This will be ignored by the MRI
+
+puts "I could have code like this" # and the comment is ignored
+
+# I could disable code if put behind the pound
+# puts "This won't run."
+puts "This will run."
